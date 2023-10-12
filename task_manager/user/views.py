e98cc999146b3ffb7d84a6f45d79a1a8c46fe859
DREU007 <dreu007@gmail.com>
2023-10-12 20:07:47 +0400

    Update urls.py views.py

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from . import forms
 
 class UsersView(View):
+    """Users page view."""
+
     def get(self, request, *args, **kwargs):
+        """Return all users."""
         users = User.objects.all() 
         return render(request, 'user/index.html', {'users': users})
+
+
+class UserCreateView(View):
+    """User create view."""
+
+    def get(self, request, *args, **kwargs):
+        """Return registration form."""
+        form = UserCreationForm()
+        return render(request, 'user/form.html', {'form': form})

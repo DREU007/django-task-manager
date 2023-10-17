b4dfa1c7355243ae74c309030b1b5937a272f6dc
DREU007 <dreu007@gmail.com>
2023-10-17 13:58:26 +0400

    Update urls.py index.html

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -15,11 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path, include, reverse_lazy
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name="index"),
+    path('', views.IndexView.as_view(), name='index'),
+    path(
+        'login/',
+        LoginView.as_view(template_name='user/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(next_page=reverse_lazy('users_index')),
+        name='logout'
+    ),
     path('users/', include('task_manager.user.urls')),
     path('admin/', admin.site.urls),
 ]

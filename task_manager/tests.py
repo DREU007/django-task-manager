565b8abe19ade1da7f744d3a3954f74f9368780b
Denis Bolshakov <dreu007@gmail.com>
2023-10-31 13:19:59 +0400

    Update tests.py

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -1,7 +1,40 @@
 from django.test import Client, TestCase
+from django.shortcuts import reverse
 
 
-class RegistrationPageViewTest(TestCase):
+class HomePageTest(TestCase):
     """Test registration page."""
+    
+    def setUp(self):
+        """Set initial condition for each test method."""  
+        self.client = Client()
+
+    def test_home_page_view(self):
+        """Test home page 200 status."""
+        url = reverse('index')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
 
+class UserCRUDTest(TestCase):
+    """Test pages for user CRUD operations."""
+    
+    def setUp(self):
+        """Set initial condition for each test method."""  
+        client = Client()
+        data = {
+                'username': 'tota',
+                'first_name': 'Tota',
+                'last_name': 'Totavich',
+                'password1': 'adminadmin',
+                'password2': data['password1'],
+        }
+
+    def test_create_user_view(self):
+        """Test user creation view."""
+        url = reverse('user_create')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_create_user_form(self):
+        k1

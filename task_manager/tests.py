9a71af6c1723595b52698d8469312bc1de31220e
Denis Bolshakov <dreu007@gmail.com>
2023-11-04 09:21:30 +0400

    Update tests.py

--- task_manager/tests.py
+++ task_manager/tests.py
@@ -22,8 +22,8 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 200)
 
 
-class UserCRUDTest(TestCase):
-    """Test pages for user CRUD operations."""
+class UserCreateTest(TestCase):
+    """Test user creation operations."""
     
     def setUp(self):
         """Set initial condition for each test method."""  
@@ -80,4 +80,51 @@ class UserCRUDTest(TestCase):
         self.assertEqual(user['first_name'], new_user.first_name)
         self.assertEqual(user['last_name'], new_user.last_name)
 
-    
+
+class UserRUDTest(TestCase):
+    """Test user read, update, delete operations."""
+
+    def setUp(self):
+        """Set initial condition for each test method."""  
+        self.client = Client()
+        self.data = {
+            'user1': {
+                'username': 'tota',
+                'first_name': 'Tota',
+                'last_name': 'Totavich',
+                'password': 'adminadmin',
+            },
+            'user2': {
+                'username': 'dada',
+                'first_name': 'Dada',
+                'last_name': 'Dadavich',
+                'password': 'adminadmin',
+            },
+        }
+
+    def test_read_user(self):
+        """Test user present on view."""
+        url = reverse('users_index')
+        user = self.data['user1']
+        User.objects.create(**user)
+
+        response = self.client.get(url)
+        response_text = response.content.decode('utf-8')
+
+        self.assertIn(user['username'], response_text) 
+        self.assertIn(user['first_name'], response_text) 
+        self.assertIn(user['last_name'], response_text) 
+
+    def test_update_user(self):
+        """Test user update data."""
+        user = self.data['user1']
+        user_updated = self.data['user2'] 
+        
+        new_user = User.objects.create(**user)
+        url = reverse('user_update', 'pk'=new_user.pk)
+
+        response = self.client.post(url, user_updated)
+        
+        self.assertEqual(user_updated['username'], new_user.username)
+        self.assertEqual(user_updated['first_name'], new_user.first_name)
+        self.assertEqual(user_updated['last_name'], new_user.last_name)

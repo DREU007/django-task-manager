554bac5983b0cd71ad01316acc1c14df7d372ffd
Denis Bolshakov <dreu007@gmail.com>
2023-11-05 20:56:23 +0400

    Add status/ app
    Changes to be committed:
    	new file:   task_manager/status/__init__.py
    	new file:   task_manager/status/admin.py
    	new file:   task_manager/status/apps.py
    	new file:   task_manager/status/migrations/__init__.py
    	new file:   task_manager/status/models.py
    	new file:   task_manager/status/tests.py
    	new file:   task_manager/status/urls.py
    	new file:   task_manager/status/views.py

---
+++ task_manager/status/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.StatusView.as_view(), name='status_index'),
+    path('create/', views.StatusCreateView.as_view(), name='status_create'),
+    path('<int:pk>/update', views.StatusUpdateView.as_view(), name='status_update'),
+    path('<int:pk>/delete', views.StatusDeleteView.as_view(), name='status_delete'),
+]

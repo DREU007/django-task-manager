5db8b3ba3c04e0f04522cedcbb1d0ed889acdde3
Denis Bolshakov <dreu007@gmail.com>
2023-11-06 19:48:32 +0400

    Fix urls.py: change class View name

--- task_manager/status/urls.py
+++ task_manager/status/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.StatusView.as_view(), name='status_index'),
-    path('create/', views.StatusCreateView.as_view(), name='status_create'),
-    path('<int:pk>/update', views.StatusUpdateView.as_view(), name='status_update'),
-    path('<int:pk>/delete', views.StatusDeleteView.as_view(), name='status_delete'),
+    path('', views.StatusIndexView.as_view(), name='status_index'),
+    #    path('create/', views.StatusCreateView.as_view(), name='status_create'),
+    #    path('<int:pk>/update', views.StatusUpdateView.as_view(), name='status_update'),
+    #    path('<int:pk>/delete', views.StatusDeleteView.as_view(), name='status_delete'),
 ]

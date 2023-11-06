fee28684e9164a7b99d55cdd92498f06817b3340
Denis Bolshakov <dreu007@gmail.com>
2023-11-06 13:38:50 +0400

    Feat views.py

--- task_manager/user/views.py
+++ task_manager/user/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from django.contrib import messages
 from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 from . import forms
+
+
+class UserLimitChangeMixin(UserPassesTestMixin):
+    """Limit the user ability to change other users."""
+
+    def test_func(self):
+        """Check if the user ID match with ID extracted from URL parameters."""
+        user_id = self.kwargs.get('pk')
+        return self.request.user.pk == user_id
+
+    def handle_no_permission(self):
+        """Redirect to users index with flash msg."""
+        msg_text = _("You don't have permition to change other user.")
+        messages.error(self.request, msg_text)
+        return redirect('users_index')
 
 
 class UsersView(View):
@@ -37,20 +52,13 @@ class UserCreateView(View):
         return render(request, 'user/create.html', {'form': form}) 
 
 
-class UserUpdateView(LoginRequiredMixin, View):
+class UserUpdateView(LoginRequiredMixin, UserLimitChangeMixin, View):
     """User update page view."""
     login_url = reverse_lazy('login')
 
     def get(self, request, *args, **kwargs):
         """Return a user data filled form."""
         user_id = kwargs.get('pk')
-        # It's better to make mixin with method Permit denied
-        logged_user = request.user
-        if logged_user.pk != user_id:
-            msg_text = _("You don't have permition to change other user.")
-            messages.error(request, msg_text)
-            return redirect('users_index')
-
         user = get_object_or_404(User, pk=user_id)
         form = forms.CustomUserChangeForm(user=user, instance=user)
         return render(
@@ -76,10 +84,9 @@ class UserUpdateView(LoginRequiredMixin, View):
         )
 
 
-class UserDeleteView(LoginRequiredMixin, View):
+class UserDeleteView(LoginRequiredMixin, UserLimitChangeMixin, View):
     """User delete page view."""
     login_url = reverse_lazy('login')
-
 
     def get(self, request, *args, **kwargs):
         """Render delete user template."""

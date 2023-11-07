c6a65e3e49dad17d83442d1a150e1b68968f429c
Denis Bolshakov <dreu007@gmail.com>
2023-11-07 10:24:58 +0400

    Update models.py: extend Status model

--- task_manager/status/models.py
+++ task_manager/status/models.py
@@ -1,6 +1,15 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=255)
-
+    """Task status one-to-many tasks.""" 
+    name = models.CharField(
+        _("name"),
+        max_length=255,
+        unique=True,
+        help_text=_("Required 255 characters or fewer."),
+        error_messages={
+            "unique": _("Task status with such Name already exist."),
+        }
+    )

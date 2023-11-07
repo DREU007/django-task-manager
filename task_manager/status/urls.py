1875f1db844fb91e0ac208dbf31cb16daec96d78
Denis Bolshakov <dreu007@gmail.com>
2023-11-07 17:07:05 +0400

    Update urls.py: include all views

--- task_manager/status/urls.py
+++ task_manager/status/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.StatusIndexView.as_view(), name='status_index'),
-    #    path('create/', views.StatusCreateView.as_view(), name='status_create'),
-    #    path('<int:pk>/update', views.StatusUpdateView.as_view(), name='status_update'),
-    #    path('<int:pk>/delete', views.StatusDeleteView.as_view(), name='status_delete'),
+    path('create/', views.StatusCreateView.as_view(), name='status_create'),
+    path('<int:pk>/update', views.StatusUpdateView.as_view(), name='status_update'),
+    path('<int:pk>/delete', views.StatusDeleteView.as_view(), name='status_delete'),
 ]

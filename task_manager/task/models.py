b67c9200dfb8c2c43ea7dec81a31a71a85d9feb4
Denis Bolshakov <dreu007@gmail.com>
2023-11-10 16:31:12 +0400

    Update tests.py: task CRUD tests

--- task_manager/task/models.py
+++ task_manager/task/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _ 
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(
+        _('name'),
+        max_length=255,
+        unique=True,
+        help_text=_("Required 255 characters or fewer."),
+        error_messages={
+            "unique": _("Task status with such Name already exist."),
+        }
+    )
+
+    description = models.TextField(
+        _('description'),
+        max_length=255,
+        unique=False,
+        help_text=_("Required 255 characters or fewer."),
+        error_messages={
+            "unique": _("Task status with such Name already exist."),
+        }
+    )
+
+    author = models.ForeignKey(
+        'User',
+        on_delete=models.PROTECT,
+    )
+        
+    status = models.ForeignKey(
+        "task_manager.status.Status",
+        on_delete=models.DO_NOTHING,
+    )
+
+    executor = models.ForeignKey(
+        "User",
+        on_delete=models.PROTECT,
+    )
+
+    # tag = models.ForeignKey(
+    #     'task_manager.tag.Tag',
+    #     on_delete=models.DO_NOTHING,
+    # )
+
+    created_at = models.DateTimeField(
+        _('Created at'),
+        auto_now_add=True,
+    )
+    
+    updated_at = models.DateTimeField(
+        _('Updated at'),
+        auto_now=True,
+    )

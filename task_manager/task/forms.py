dabab59b46c5d4688bbf70eef4e6a5a059423335
Denis Bolshakov <dreu007@gmail.com>
2023-11-11 08:48:16 +0400

    Add forms.py: TaskForm

---
+++ task_manager/task/forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from .import models
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = models.Task
+        fields = [
+            'name',
+            'description',
+            'status',
+            'executor',
+            # 'tag',
+        ]

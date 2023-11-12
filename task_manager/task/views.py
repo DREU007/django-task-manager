a9313a7fad5af4a76a4fb597375d2275288cd300
Denis Bolshakov <dreu007@gmail.com>
2023-11-12 18:32:09 +0400

    Feat views.py: task Delete view class

--- task_manager/task/views.py
+++ task_manager/task/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
-from django.urls import reverse_lazy
 from django.utils.translation import gettext as _
 
 from .models import Task
@@ -17,7 +16,7 @@ class TaskIndexView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         """Return tasks index."""
         tasks = Task.objects.all()
-        return render(request, template, {'tasks': tasks})
+        return render(request, self.template, {'tasks': tasks})
 
 
 class TaskCreateView(LoginRequiredMixin, View):
@@ -28,22 +27,20 @@ class TaskCreateView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
         """Return task creation form."""
         form = TaskForm()
-        return render(request, template, {'form': form})
+        return render(request, self.template, {'form': form})
 
     def post(self, request, *args, **kwargs):
         """Create task on POST."""
         form = TaskForm(request.POST)
         if form.is_valid():
             form.save()
-            msg_text = _('Task is successefully created')
+            msg_text = _('Task successefully created')
             messages.success(request, msg_text)
             return redirect('tasks_index')
-        msg_text = _('Somthing wrong') # TODO:
-        messages.error(request, msg_text)
-        return render(request, template, {'form': form})
+        return render(request, self.template, {'form': form})
 
 
-class TaskUpdateView(LoginRequiredMixin, View): 
+class TaskUpdateView(LoginRequiredMixin, View):
     """Task updation view."""
     login_url = 'login'
     template = 'task/update.html'
@@ -53,7 +50,7 @@ class TaskUpdateView(LoginRequiredMixin, View):
         task_pk = kwargs.get('pk')
         task = get_object_or_404(Task, pk=task_pk)
         form = TaskForm(Task, instance=task)
-        return render(request, template, {'form': form})
+        return render(request, self.template, {'form': form})
 
     def post(self, request, *args, **kwargs):
         """Update task on POST."""
@@ -62,9 +59,28 @@ class TaskUpdateView(LoginRequiredMixin, View):
         form = TaskForm(instance=task)
         if form.is_valid():
             form.save()
-            msg_text = _('Successfully updated task')
+            msg_text = _('Task successfully updated')
             messages.success(request, msg_text)
             return redirect('tasks_index')
-        msg_text = _('Somthing wrong') # TODO:
-        messages.error(request, msg_text)
-        return render(request, template, {'form': form})
+        return render(request, self.template, {'form': form})
+
+
+class TaskDeleteView(LoginRequiredMixin, View):
+    """Task deletion view."""
+    login_url = 'login'
+    template = 'task/delete.html'
+
+    def get(self, request, *args, **kwargs):
+        """Retern task delete form."""
+        task_pk = kwargs.get('pk')
+        task = get_object_or_404(Task, instance=task_pk)
+        return render(request, self.template, {'task': task})
+
+    def post(self, request, *args, **kwargs):
+        """Delete task on POST."""
+        task_pk = kwargs.get('pk')
+        task = get_object_or_404(Task, pk=task_pk)
+        task.delete()
+        msg_text = _('Task successfully deleted')
+        messages.success(request, msg_text)
+        return redirect('tasks_index')

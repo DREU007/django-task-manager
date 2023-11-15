1bbf41e656075f1e901de4410b51a743037edfc2
Denis Bolshakov <dreu007@gmail.com>
2023-11-15 18:37:12 +0400

    Update models.py: created_at updated_at

--- task_manager/label/models.py
+++ task_manager/label/models.py
@@ -16,6 +16,14 @@ class Label(models.Model):
         }
     )
     task = models.ManyToManyField(Task)
+    created_at = models.DateTimeField(
+        _('created at'),
+        auto_now_add=True,
+    )
+    updated_at = models.DateTimeField(
+        _('updated_at'),
+        auto_now=True,
+    )
 
     def get_absolute_url(self):
         return reverse('labels_index')

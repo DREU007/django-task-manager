44d3236ecda6d0dd50dc9084e59a9e91ac5ae9c6
Denis Bolshakov <dreu007@gmail.com>
2023-11-15 17:49:28 +0400

    Feat views.py: label CRUD operations

--- task_manager/label/views.py
+++ task_manager/label/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView,
+    ModelFormMixin
+)
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Label
+
+
+class LabelModelMixin(ModelFormMixin):
+    model = Label
+    fields = ['name']
+
+
+class LabelIndexView(LoginRequiredMixin, View):
+    """Label index view."""
+    login_url = reverse_lazy('login')
+    template = 'label/index.html'
+
+    def get(self, request, *args, **kwargs):
+        """Return label index template."""
+        labels = Label.objects.all()
+        return render(request, self.template, {'labels': labels})
+
+
+class LabelCreateView(LoginRequiredMixin, LabelModelMixin, CreateView):
+    login_url = reverse_lazy('login')
+    template_name = 'label/create.html'
+
+
+class LabelUpdateView(LoginRequiredMixin, LabelModelMixin, UpdateView):
+    login_url = reverse_lazy('login')
+    template_name = 'label/update.html'
+
+
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
+    success_url = reverse_lazy('labels_index')
+    template_name = 'label/delete.html'
+
+    model = Label

3d462b2b6aff354e2c3b9a238a8f26eb41be822c
Denis Bolshakov <dreu007@gmail.com>
2023-11-15 15:18:21 +0400

    Update models.py: create Label model

--- task_manager/label/models.py
+++ task_manager/label/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from task_manager.task.models import Task
 
-# Create your models here.
+
+class Label(models.Model):
+    """Represents a label for a task in the task manager."""
+    name = models.CharField(
+        _('name'),
+        max_length=255,
+        unique=True,
+        help_text=_("Required 255 characters or fewer."),
+        error_messages={
+            "unique": _("Task status with such Name already exist."),
+        }
+    )
+    task = models.ManyToManyField(
+       Task,
+       on_delete=models.PROTECT,
+    )

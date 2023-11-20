ff5f354385096bfbbf9bc8896416bcde55f40bcf
Denis Bolshakov <dreu007@gmail.com>
2023-11-20 12:18:25 +0400

    Feat settings.py: rollback notification

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -77,7 +77,15 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'rollbar.contrib.django.middleware.RollbarNotifierMiddleware',
 ]
+
+ROLLBAR = {
+    'access_token': os.getenv('ROLLBAR_TOKEN', False),
+    'environment': 'development' if DEBUG else 'production',
+    'code_version': '1.0',
+    'root': BASE_DIR,
+}
 
 ROOT_URLCONF = 'task_manager.urls'
 

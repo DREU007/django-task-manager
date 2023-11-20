c883c9e641a2c18db21ed2af2eb453e5e9838d60
Denis Bolshakov <dreu007@gmail.com>
2023-11-20 19:40:32 +0400

    Update settings.py: language code ru

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -149,7 +149,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'ru-RU'
 
 LANGUAGES = [
     ('en', 'English'),

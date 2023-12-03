090f5b10137573b6543397065905b9ef6c9fc2d9
Denis Bolshakov <dreu007@gmail.com>
2023-12-03 11:48:21 +0400

    Update views.py: DeleteView & refactor

--- task_manager/label/views.py
+++ task_manager/label/views.py
@@ -7,12 +7,19 @@ from django.views.generic.edit import (
 )
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
+from django.http import HttpResponseRedirect
 
 from .models import Label
 
 
-class LabelModelMixin(ModelFormMixin):
+class LabelModelMixin(LoginRequiredMixin, SuccessMessageMixin, ModelFormMixin):
+    login_url = reverse_lazy('login')
+    success_url = reverse_lazy('labels_index')
+
     model = Label
     fields = ['name']
 
@@ -28,19 +35,39 @@ class LabelIndexView(LoginRequiredMixin, View):
         return render(request, self.template, {'labels': labels})
 
 
-class LabelCreateView(LoginRequiredMixin, LabelModelMixin, CreateView):
-    login_url = reverse_lazy('login')
+class LabelCreateView(LabelModelMixin, CreateView):
+    """Label create view."""
     template_name = 'label/create.html'
+    success_message = _('Label is successfully created')
 
 
-class LabelUpdateView(LoginRequiredMixin, LabelModelMixin, UpdateView):
-    login_url = reverse_lazy('login')
+class LabelUpdateView(LabelModelMixin, UpdateView):
+    """Label update view."""
     template_name = 'label/update.html'
+    success_message = _('Label is successfully updated')
 
 
 class LabelDeleteView(LoginRequiredMixin, DeleteView):
-    login_url = reverse_lazy('login')
-    success_url = reverse_lazy('labels_index')
+    """Label delete view."""
     template_name = 'label/delete.html'
+    success_url = reverse_lazy('labels_index')
 
     model = Label
+
+    def form_valid(self, form):
+        """
+        Custom delete logic on POST.
+
+        Call the delete() method on the fetched object, create
+        the success message and redirect to the success URL.
+
+        Copy behaivor of django.views.generic.edit.DeletionMixin but
+        with the success_message.
+        """
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.delete()
+
+        msg_txt = _('Label is successfully deleted')
+        messages.success(self.request, msg_txt)
+        return HttpResponseRedirect(success_url)

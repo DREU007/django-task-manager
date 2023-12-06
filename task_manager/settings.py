5be34cca11ec2663f499daef7b9a18b0ad540fef
Denis Bolshakov <dreu007@gmail.com>
2023-12-07 00:50:29 +0400

    Fix settings.py: DEBUG default value

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(strtobool(os.getenv('DEBUG', False)))
+DEBUG = bool(strtobool(os.getenv('DEBUG', 'False')))
 
 LOCAL_HOST = os.getenv('HOST')
 
